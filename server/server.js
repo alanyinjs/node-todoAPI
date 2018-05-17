bdfc3d639668e3ef716d021e36ada300814be8f8
Alan Yin <yongqian.yin@gmail.com>
2018-05-18 09:34:01 +1000

    add GET todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,16 @@ app.post('/todos', (req, res)=>{
   })
 });
 
+app.get('/todos', (req, res)=>{
+  Todo.find().then((todos)=>{
+    res.send({
+      todos
+    });
+  }, (e)=>{
+    res.status(400).send(e);
+  })
+})
+
 let port = process.env.PORT||3000;
 
 app.listen(port, ()=>{
