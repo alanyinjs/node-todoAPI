1d241378081b8d30d850ac4835ca2150a956f2fc
Alan Yin <yongqian.yin@gmail.com>
2018-05-18 14:47:34 +1000

    add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,10 +4,13 @@ const bodyParser = require('body-parser');
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
 let {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 let app = express();
 
 app.use(bodyParser.json());
+
+// POST /todos
 
 app.post('/todos', (req, res)=>{
   let todo = new Todo({
@@ -21,6 +24,8 @@ app.post('/todos', (req, res)=>{
   });
 });
 
+// GET /todos
+
 app.get('/todos', (req, res)=>{
   Todo.find().then((todos)=>{
     res.send({
@@ -28,6 +33,26 @@ app.get('/todos', (req, res)=>{
     });
   }, (e)=>{
     res.status(400).send(e);
+  });
+});
+
+// GET /todos/:id
+
+app.get('/todos/:id', (req, res)=>{
+  let id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+    res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
   });
 });
 
